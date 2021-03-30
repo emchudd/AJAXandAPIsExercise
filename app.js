8f795d4890b4539a63278075814928a298a0bac8
emchudd <eclhuddleston@gmail.com>
2021-03-30 08:56:48 -0500

    completed exercise and added bonus from class review

---
+++ app.js
@@ -0,0 +1,35 @@
+// AJAX and APIs Exercise
+// 1
+const first = document.querySelector('#first');
+const p1 = document.createElement('p');
+const p2 = document.createElement('p');
+first.append(p1);
+first.append(p2);
+//1a
+const jokeJS1 = `{"id":18,"type":"programming","setup":"Why did the programmer quit his job?","punchline":"Because he didn't get arrays."}`;
+console.log(jokeJS1);
+const jokeJS = JSON.parse(jokeJS1);
+console.log(jokeJS);
+//1b
+p1.innerText(jokeJS.setup);
+p2.innerText(jokeJS.punchline);
+//console.log(jokeJS.setup);
+//console.log(jokeJS.punchline);
+jokeJS();
+
+// 2
+const second = document.querySelector('#second');
+const p3 = document.createElement('p');
+const p4 = document.createElement('p');
+second.append(p3);
+second.append(p4);
+// 3
+const third = document.querySelector('#third');
+const p5 = document.createElement('p');
+const p6 = document.createElement('p');
+third.append(p5);
+third.append(p6);
+// 4
+const fourth = document.querySelector('#fourth');
+const p7 = document.createElement('p');
+fourth.append(p7);
